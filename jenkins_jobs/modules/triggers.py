16c69e673e0f95986969ca8351a8a1bb48508c81
Herbert Pfennig <dasein555@gmail.com>
2020-03-04 16:25:15 -0800

    Gitlab support for source-branch-regex functionality

--- jenkins_jobs/modules/triggers.py
+++ jenkins_jobs/modules/triggers.py
@@ -1401,7 +1401,8 @@ def gitlab(registry, xml_parent, data):
         (default [])
     :arg list exclude-branches: Defined list of branches to exclude
         (default [])
-    :arg str target-branch-regex: Regular expression to select branches
+    :arg str target-branch-regex: Regular expression to select target branches
+    :arg str source-branch-regex: Regular expression to select source branches
     :arg str secret-token: Secret token for build trigger
 
     .. _`branch filter type`:
@@ -1501,6 +1502,7 @@ def gitlab(registry, xml_parent, data):
         ("add-ci-message", "addCiMessage", False),
         ("allow-all-branches", "allowAllBranches", False),
         ("target-branch-regex", "targetBranchRegex", ""),
+        ("source-branch-regex', 'sourceBranchRegex", ""),
         ("secret-token", "secretToken", ""),
     ]
 
